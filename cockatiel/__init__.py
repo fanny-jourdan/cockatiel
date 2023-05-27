0ab65ad39d5aed1cc574b4387c55d617d253f1c9
agustin-martin.picard <agustin-martin.picard@irt-saintexupery.com>
2023-05-27 13:01:05 +0200

    all: fix imports on init

--- cockatiel/__init__.py
+++ cockatiel/__init__.py
@@ -1,4 +1,6 @@
 from .cockatiel import COCKATIEL
+from .RobertaModel import CustomRobertaForSequenceClassification
 from .utils import batcher, tokenize, preprocess, batch_predict
 from .occlusion import occlusion_concepts
 from .sobol import SobolEstimator, JansenEstimator
+from .visualisations import viz_concepts, print_legend
